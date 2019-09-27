a9272f3e932bf92a35278ebddac81746e9e87cc5
xiaohuge <xiaohuge@xiaohugedeMBP.lan>
2019-09-28 03:27:01 +0800

    f

---
+++ config/unicorn.rb
@@ -0,0 +1,54 @@
+module Rails
+  class << self
+    def root
+      File.expand_path("../..", __FILE__)
+    end
+  end
+end
+
+APP_HOME = Rails.root
+
+worker_processes 2
+
+working_directory APP_HOME # available in 0.94.0+
+
+if 'production' == ENV['RAILS_ENV']
+  listen "/data/www/wblog/tmp/sockets/unicorn.sock", :backlog => 64
+  pid "/data/www/wblog/tmp/pids/unicorn.pid"
+else
+  listen 3006, :tcp_nopush => true
+  pid "#{APP_HOME}/tmp/pids/unicorn.pid"
+end
+
+timeout 60
+
+stderr_path "#{APP_HOME}/log/unicorn.stderr.log"
+stdout_path "#{APP_HOME}/log/unicorn.stdout.log"
+
+preload_app true
+
+if GC.respond_to?(:copy_on_write_friendly=)
+  GC.copy_on_write_friendly = true
+end
+
+check_client_connection false
+
+before_fork do |server, worker|
+  defined?(ActiveRecord::Base) and
+    ActiveRecord::Base.connection.disconnect!
+
+  old_pid = "#{APP_HOME}/tmp/pids/unicorn.pid.oldbin"
+  if File.exists?(old_pid) && server.pid != old_pid
+    begin
+      Process.kill("QUIT", File.read(old_pid).to_i)
+    rescue Errno::ENOENT, Errno::ESRCH
+      puts "Send 'QUIT' signal to unicorn error!"
+    end
+  end
+end
+
+after_fork do |server, worker|
+  defined?(ActiveRecord::Base) and
+    ActiveRecord::Base.establish_connection
+end
+
