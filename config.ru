2ca272246fde15a39bf511db87e50f86ff0565cd
xiaohuge <xiaohuge@xiaohugedeMBP.lan>
2019-09-28 03:12:56 +0800

    fix b

--- config.ru
+++ config.ru
@@ -1,14 +1,3 @@
 require ::File.expand_path('../config/environment',  __FILE__)
 run WBlog::Application
 
-require 'rack/cors'
-use Rack::Cors do
-
-  # allow all origins in development
-  allow do
-    origins '*'
-    resource '*',
-             :headers => :any,
-             :methods => [:get, :post, :delete, :put, :options]
-  end
-end
--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,17 @@
 require_relative 'boot'
 
 require 'rails/all'
+require 'rack/cors'
+use Rack::Cors do
+
+  # allow all origins in development
+  allow do
+    origins '*'
+    resource '*',
+             :headers => :any,
+             :methods => [:get, :post, :delete, :put, :options]
+  end
+end
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
