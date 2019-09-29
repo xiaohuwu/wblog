87f82596f7f89fee21f1198878e022091451a700
xiaohu <1205150638@qq.com>
2019-09-29 16:10:27 +0800

    fix bug

--- config/deploy.rb
+++ config/deploy.rb
@@ -97,7 +97,7 @@ namespace :unicorn do
   desc "Restart unicorn"
   task :restart => :environment do
     invoke 'unicorn:stop'
-    #invoke 'unicorn:start'
+    invoke 'unicorn:start'
   end
 end
 
