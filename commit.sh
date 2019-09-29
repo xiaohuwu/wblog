41813fee6c5534c20768bcf7b8d124ffce496bff
xiaohuge <xiaohuge@xiaohugedeMBP.lan>
2019-09-30 00:23:34 +0800

    fix

---
+++ commit.sh
@@ -0,0 +1,4 @@
+git add .
+git commit -m "fix"
+git pull origin master
+git push origin master
--- config/unicorn.rb
+++ config/unicorn.rb
@@ -6,7 +6,7 @@ module Rails
   end
 end
 
-APP_HOME = '/data/www/wblog' #Rails.root
+APP_HOME =  Rails.root
 puts "APP_HOME: #{APP_HOME}"
 worker_processes 2
 
