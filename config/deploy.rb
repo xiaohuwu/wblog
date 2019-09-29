3538bc8611d026c10c7a2440aa46a78537c39ba7
xiaohuge <xiaohuge@xiaohugedeMBP.lan>
2019-09-30 00:06:31 +0800

    fix bug

--- config/deploy.rb
+++ config/deploy.rb
@@ -25,8 +25,8 @@ set :port, '3118'           # SSH port number.
 # Shared dirs and files will be symlinked into the app-folder by the 'deploy:link_shared_paths' step.
 # Some plugins already add folders to shared_dirs like `mina/rails` add `public/assets`, `vendor/bundle` and many more
 # run `mina -d` to see all folders and files already included in `shared_dirs` and `shared_files`
-set :shared_dirs, fetch(:shared_dirs, []).push('public/assets','tmp')
-set :shared_files, fetch(:shared_files, []).push('config/database.yml', 'config/secrets.yml','tmp/sockets','tmp/pids','config/application.yml')
+set :shared_dirs, fetch(:shared_dirs, []).push('public/assets')
+set :shared_files, fetch(:shared_files, []).push('config/database.yml', 'config/secrets.yml','config/application.yml')
 #set :sidekiq_pid, "/data/www/wblog/tmp/pids/sidekiq.pid"
 #set :unicorn_pid, "/data/www/wblog/tmp/pids/unicorn.pid"
 
